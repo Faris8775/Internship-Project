4b3ce98df2452718733d749e12152b2a61ea2cb4
Faris Alkassim <faruka16@gmail.com>
2024-09-05 18:00:20 -0400

    Internship Project Task 5 -Adding a reporting tool Commit

--- features/environment.py
+++ features/environment.py
@@ -16,9 +16,9 @@ def browser_init(context, scenario_name):
     # Chrome Web Browser
     #driver_path = ChromeDriverManager().install()
 
-    # driver_path = r'C:\Users\faruk\Downloads\internship-project\chromedriver.exe'
-    # service = Service(driver_path)
-    # context.driver = webdriver.Chrome(service=service)
+    driver_path = r'C:\Users\faruk\Downloads\internship-project\chromedriver.exe'
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service)
 
     ### HEADLESS MODE ####
     # options = webdriver.ChromeOptions()
@@ -32,22 +32,22 @@ def browser_init(context, scenario_name):
 
     ### BROWSERSTACK ###
     # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
-    bs_user = 'farisalkassim_J1hq9x'
-    bs_key = 'rxZS5zUJeLaq2Amv4bHA'
-    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-
-    options = Options()
-    bstack_options = {
-        "os" : "OS X",
-        "osVersion" : "Sequoia",
-        'browserName': 'Firefox',
-        'browserVersion': 'latest',
-        'sessionName': 'scenario_name',
-        # 'buildName': 'alpha_0.1.1',
-        # 'projectName': 'User can see titles and pictures on each product inside the off plan page'
-    }
-    options.set_capability('bstack:options', bstack_options)
-    context.driver = webdriver.Remote(command_executor=url, options=options)
+    # bs_user = 'farisalkassim_J1hq9x'
+    # bs_key = 'rxZS5zUJeLaq2Amv4bHA'
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #     "os" : "OS X",
+    #     "osVersion" : "Sequoia",
+    #     'browserName': 'Firefox',
+    #     'browserVersion': 'latest',
+    #     'sessionName': 'scenario_name',
+    #     # 'buildName': 'alpha_0.1.1',
+    #     # 'projectName': 'User can see titles and pictures on each product inside the off plan page'
+    # }
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
 
 
     # Firefox Web Browser
